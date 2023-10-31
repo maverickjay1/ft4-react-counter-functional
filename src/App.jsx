95e278c7d008954c03ca94ba20e87b2b400aeb30
JM <maverick@Js-MacBook-Pro-14.local>
2023-10-31 23:44:59 +0000

    added useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
 
   const plus = () => {
     setCount(count + 1);
@@ -14,53 +15,23 @@ function Counter() {
     setCount(count - 1);
   };
 
+  useEffect(() => {
+    if (count % 10 === 0 && count > 0) {
+      setMessage(`You've reached ${count}`);
+    } else {
+      setMessage("");
+    }
+  }, [count]);
+
   return (
     <div>
       <h1>Counter App</h1>
       <p>Count: {count}</p>
       <button onClick={plus}>Plus</button>
       <button onClick={minus}>Minus</button>
+      <p>{message}</p>
     </div>
   );
 }
 
 export default Counter;
-
-// import { React, useState } from "react";
-
-// export default function App() {
-//   const [counter, setCounter] = useState(0);
-
-//   //increase counter
-//   const increase = () => {
-//     setCounter((count) => count + 1);
-//   };
-
-//   //decrease counter
-//   const decrease = () => {
-//     setCounter((count) => count - 1);
-//   };
-
-//   //reset counter
-//   const reset = () => {
-//     setCounter(0);
-//   };
-
-//   return (
-//     <div className="counter">
-//       <h1>React Counter</h1>
-//       <span className="counter__output">{counter}</span>
-//       <div className="btn__container">
-//         <button className="control__btn" onClick={increase}>
-//           +
-//         </button>
-//         <button className="control__btn" onClick={decrease}>
-//           -
-//         </button>
-//         <button className="reset" onClick={reset}>
-//           Reset
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
